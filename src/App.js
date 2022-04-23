f2cc99110d92dc2f52124ffad2ac2d94332cc2c9
karvaroz <vargasriosk@gmail.com>
2022-04-23 16:34:27 -0500

    Estilos listos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <>
+      <div className="container">
+        <div className="stars">
+          <img src="./images/bg-stars.svg" alt="" />
+        </div>
+        <h1>We're launching soon</h1>
+        <div className="countdown">
+          <div>
+            <p>00</p>
+            <h3>Days</h3>
+          </div>
+          <div>
+            <p>00</p>
+            <h3>Hours</h3>
+          </div>
+          <div>
+            <p>00</p>
+            <h3>Minutes</h3>
+          </div>
+          <div>
+            <p>00</p>
+            <h3>Seconds</h3>
+          </div>
+        </div>
+        <div className="socials">
+          <ul>
+            <li>
+              <img src="./images/icon-facebook.svg" alt="" />
+            </li>
+            <li>
+              <img src="./images/icon-pinterest.svg" alt="" />
+            </li>
+            <li>
+              <img src="./images/icon-instagram.svg" alt="" />
+            </li>
+          </ul>
+        </div>
+        <div className="hills">
+          <img src="./images/pattern-hills.svg" alt="" />
+        </div>
+      </div>
+
+      <div class="attribution">
+        Challenge by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
-    </div>
+        . Coded by{" "}
+        <a href="https://github.com/karvaroz" rel="noreferrer">
+          Karvaroz
+        </a>
+        .
+      </div>
+    </>
   );
 }
 
